68d9fd8c36c12936c8b6a28e48e9183901e64474
Michael Ruhl <michaelruhl14@hotmail.com>
2023-07-20 14:58:50 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const 
+
+
+</script>
+
+<template>
+  <div>
+   Hello World
+  </div>
+  
+</template>
+
+
